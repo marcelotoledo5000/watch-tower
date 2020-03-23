48f6132ca9eed0f06bb6a605c27a29b19465da6c
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-03-23 01:25:30 -0300

    Update stores_spec.rb
    Add exceptions

--- spec/requests/stores_spec.rb
+++ spec/requests/stores_spec.rb
@@ -26,5 +26,34 @@ describe 'Stores', type: :request do
         expect(json[:name]).to eq 'Maximus Store'
       end
     end
+
+    context 'when is bad request' do
+      it 'returns status code 400' do
+        expect{ post stores_path }.
+          to raise_error ActionController::ParameterMissing
+      end
+    end
+
+    context 'when the request is invalid' do
+      before do
+        post stores_path,
+             params: { store: { cnpj: ''} }
+      end
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status :unprocessable_entity
+        expect(json).not_to be_empty
+      end
+    end
+
+    xcontext 'when the user is unauthorized' do
+      before do
+        post stores_path,
+             params: valid_params
+      end
+
+      it { expect(response).to have_http_status :unauthorized }
+    end
+  end
   end
 end
