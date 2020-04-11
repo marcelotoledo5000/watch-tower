0328a4b9ef72ab61ca6fb2dd4155ca00549e2571
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-04-11 19:28:47 -0300

    Add Appointment Model

---
+++ spec/models/appointment_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Appointment, type: :model do
+  subject(:appointment) { create(:appointment) }
+
+  context 'with associations' do
+    it { is_expected.to belong_to(:visitor).inverse_of(:appointments) }
+    it { is_expected.to belong_to(:store).inverse_of(:appointments) }
+  end
+
+  context 'with validations' do
+    it { is_expected.to validate_presence_of(:event_time) }
+  end
+
+  context 'with enum' do
+    it do
+      expect(appointment).to define_enum_for(:kind).
+        with_values(check_in: 0, check_out: 1)
+    end
+  end
+end
