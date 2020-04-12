df3e7eaaa86f5e3c08be22842802331b663f084d
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-04-12 12:40:11 -0300

    Update authentication_spec.rb
    Improves to Authentication tests

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -3,20 +3,19 @@
 require 'rails_helper'
 
 describe 'Authentication', type: :request do
-  describe 'POST /login', type: :request do
-    let(:user) { create(:user) }
-    let(:params) do
-      {
-        user: {
-          email: user.email,
-          password: user.password
-        }
+  let(:devise_key) { ENV['DEVISE_JWT_SECRET_KEY'] }
+  let(:user) { create(:user) }
+  let(:params) do
+    {
+      user: {
+        email: user.email,
+        password: user.password
       }
-    end
+    }
+  end
 
+  describe 'POST /login', type: :request do
     context 'when params are correct' do
-      let(:devise_key) { ENV['DEVISE_JWT_SECRET_KEY'] }
-
       before do
         post user_session_path, params: params
       end
@@ -43,8 +42,22 @@ describe 'Authentication', type: :request do
   end
 
   describe 'DELETE /logout', type: :request do
-    before { delete destroy_user_session_path }
+    it do
+      # create new session and get token
+      post user_session_path, params: params
+      token_from_request = response.headers['Authorization'].split(' ').last
 
-    it { expect(response).to have_http_status :no_content }
+      # delete this user session
+      delete destroy_user_session_path,
+             headers: { 'Authorization' => "Bearer #{token_from_request}" }
+
+      # decode token and get jti
+      decoded_token = JWT.decode(token_from_request, devise_key, true)
+      jti_from_token = decoded_token.first['jti']
+
+      expect(response).to have_http_status :no_content
+      expect(JwtBlacklist.count).to eq 1
+      expect(JwtBlacklist.last.jti).to eq jti_from_token
+    end
   end
 end
