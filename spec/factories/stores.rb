e80324195f220b08150e17223cfc177d54cb5e31
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-07-26 16:42:32 -0300

    Improves some factories

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :store do
-    cnpj { Faker::Company.brazilian_company_number }
+    cnpj { Faker::Company.unique.brazilian_company_number }
     name { Faker::Company.name }
   end
 end
