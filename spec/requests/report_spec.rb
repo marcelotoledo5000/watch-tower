9a0cf9f2b082406aa4d491c28c2f959a756b8e5c
Marcelo Toledo <marcelotoledo5000@gmail.com>
2021-10-26 21:25:17 -0300

    Fix Rubocop offense

--- spec/requests/report_spec.rb
+++ spec/requests/report_spec.rb
@@ -20,9 +20,7 @@ describe 'Report', type: :request do
       it 'returns the report' do
         expect(json[:total_stores]).to eq 5
         expect(json[:stores].size).to eq 5
-        expect(
-          json[:stores].pluck(:total_visitors).sum
-        ).to eq 11
+        expect(json[:stores].pluck(:total_visitors).sum).to eq 11
       end
 
       it { expect(response).to have_http_status :ok }
