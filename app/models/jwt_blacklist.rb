24c1fe03a164b61889748d2bcd4cb89679d53664
Marcelo Toledo <marcelotoledo5000@gmail.com>
2021-10-26 21:16:30 -0300

    Bump devise-jwt

--- app/models/jwt_blacklist.rb
+++ app/models/jwt_blacklist.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class JwtBlacklist < ApplicationRecord
-  include Devise::JWT::RevocationStrategies::Blacklist
+  include Devise::JWT::RevocationStrategies::Denylist
 
   self.table_name = 'jwt_blacklists'
 end
